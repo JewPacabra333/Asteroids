9b524107ef46c64ce5b2bd02b8c7a29dc0c8eb88
JewPacabra333 <benedek.laller@gmail.com>
2026-05-26 19:11:51 +0200

    rotate def

---
+++ player.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import PLAYER_RADIUS
+from constants import LINE_WIDTH
+from constants import PLAYER_TURN_SPEED
+from circleshape import CircleShape
+
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), width=LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
